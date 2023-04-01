6ce6e2b3b118cf914e9e3db39ed152f087d9db15
JuHeonHa <hjhjaueon97@naver.com>
2023-04-01 15:53:12 +0900

    네비게이션바 추가

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
+	<NavBar />
 	<router-view />
 </template>
 <script lang="ts">
 	import { defineComponent } from 'vue';
-	//import router from "./router";
+	import NavBar from './components/common/NavBar.vue';
 
 	export default defineComponent({
+		components: { NavBar },
 		setup() {
 			return {};
 		},
---
+++ src/components/common/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+	<nav>
+		<router-link :to="{ name: 'SimpleCounter' }">1.SimpleCounter</router-link>
+	</nav>
+</template>
+
+<script>
+	export default {};
+</script>
+
+<style scoped>
+	nav {
+		display: flex;
+		gap: 1rem;
+	}
+</style>
